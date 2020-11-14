8bff6aceef89d6cb13fe77ead8a7c0a027c0f061
Anwesh43 <anweshthecool0@gmail.com>
2020-11-14 21:06:06 +0530

    adding EBRNode class

--- ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
+++ ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
@@ -119,4 +119,45 @@ class ExpBarRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class EBRNode(var i : Int, val state : State = State()) {
+
+        private var next : EBRNode? = null
+        private var prev : EBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = EBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawEBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : EBRNode {
+            var curr : EBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
