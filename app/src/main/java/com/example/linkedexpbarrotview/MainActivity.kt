53506e8b29d689e86befdb7802afa1d448af7ced
Anwesh43 <anweshthecool0@gmail.com>
2020-11-15 05:24:49 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedexpbarrotview/MainActivity.kt
+++ app/src/main/java/com/example/linkedexpbarrotview/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.linkedexpbarrotview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.expbarrotview.ExpBarRotView
+import com.example.shrinkingbarview.ShrinkingBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ExpBarRotView.create(this)
+        //ExpBarRotView.create(this)
+        ShrinkingBarView.create(this)
         fullScreen()
     }
 }
