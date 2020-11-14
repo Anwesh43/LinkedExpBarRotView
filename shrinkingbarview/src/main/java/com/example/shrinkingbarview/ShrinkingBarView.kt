f8bfcb501e9a853df4869a15dd28036400b86557
Anwesh43 <anweshthecool0@gmail.com>
2020-11-15 05:03:23 +0530

    adding Canvas drawing methods

--- shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
+++ shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
@@ -29,3 +29,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawShrinkingBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf2 : Float = sf.divideScale(2, parts)
+    val barSize : Float = Math.min(w, h) / wSizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((w / 2 - barSize / 2) * (1 - sf.divideScale(1, parts)), 0f)
+        drawRect(
+            RectF(
+                -barSize / 2,
+                0f,
+                barSize / 2,
+                (h * 0.5f * sf.divideScale(0, parts) - (h / 2 - w / 2) * sf2) * (1f - 2 * j)
+            ),
+            paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawShrinkingBar(scale, w, h, paint)
+}
