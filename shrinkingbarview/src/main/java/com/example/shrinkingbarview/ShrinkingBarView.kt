e40af6edf22b11da3dae4321143bc378fc30346b
Anwesh43 <anweshthecool0@gmail.com>
2020-11-15 05:04:34 +0530

    adding ShrinkingBarView class

--- shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
+++ shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawShrinkingBar(scale, w, h, paint)
 }
+
+class ShrinkingBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
