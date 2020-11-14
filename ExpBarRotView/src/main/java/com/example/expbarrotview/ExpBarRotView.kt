63347c54290ffbab96af7c596b3e60a31bcf6461
Anwesh43 <anweshthecool0@gmail.com>
2020-11-14 19:25:40 +0530

    adding ExpBarRotView class

--- ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
+++ ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawEBRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawExpBarRot(scale, w, h, paint)
 }
+
+class ExpBarRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
