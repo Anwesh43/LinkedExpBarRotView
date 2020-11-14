a6021ad6fb0120e3a66c8ec0767486441628b335
Anwesh43 <anweshthecool0@gmail.com>
2020-11-15 04:55:17 +0530

    adding constants and libraries

--- shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
+++ shrinkingbarview/src/main/java/com/example/shrinkingbarview/ShrinkingBarView.kt
@@ -1,2 +1,27 @@
 package com.example.shrinkingbarview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val wSizeFactor : Float = 12.4f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#4CAF50",
+    "#2196F3",
+    "#FF5722"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+
