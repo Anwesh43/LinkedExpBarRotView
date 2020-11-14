2b3392c1a8d176b469971a8e42ff6eb59e233698
Anwesh43 <anweshthecool0@gmail.com>
2020-11-14 16:26:39 +0530

    adding contants

--- ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
+++ ExpBarRotView/src/main/java/com/example/expbarrotview/ExpBarRotView.kt
@@ -1,2 +1,26 @@
 package com.example.expbarrotview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#009688",
+    "#2196F3",
+    "#FF5722"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val barSizeFactor : Float = 12.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
