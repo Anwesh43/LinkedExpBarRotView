e99720054935a4ea216619dcc35f20eccf1af742
Anwesh43 <anweshthecool0@gmail.com>
2020-11-14 21:29:53 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedexpbarrotview/MainActivity.kt
+++ app/src/main/java/com/example/linkedexpbarrotview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedexpbarrotview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.expbarrotview.ExpBarRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ExpBarRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
